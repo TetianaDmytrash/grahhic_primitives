2485a5d83470e3cd023b85db0efda5a1f6558bc0
TetianaDmytrash <101727759+TetianaDmytrash@users.noreply.github.com>
2022-03-16 18:01:10 +0300

    Delete graphic_primitiver directory

--- graphic_primitiver/class_for_range.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-class class_for_range {
-public:
-	static void range_max_and_min_int(int*, int*, int*, int);
-	static void range_max_and_min_float(float*, int*, int*, int);
-	static void move_max_min_int(int*, int*, int*, int, int);
-	static void move_max_min_float(float*, int*, int*, int, int);
-};
-
